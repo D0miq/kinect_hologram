3ba4899abe4c06419dcafd3edb7c40e05984c8ae
D0miq <dpoch@email.com>
2019-04-02 19:09:09 +0200

    no issue - Show output from multiple cameras

---
+++ KinectHologram/Assets/RotateCharacter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCharacter : MonoBehaviour
+{
+    public Transform character;
+
+    float degreesPerSecond = 50.0f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * degreesPerSecond * Time.deltaTime, Space.Self);
+    }
+}
