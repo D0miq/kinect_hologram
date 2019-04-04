30964f33bd30c6e57a683c389d8970565e350e33
D0miq <dpoch@email.com>
2019-04-04 22:48:23 +0200

    Face tracking with Kinect

---
+++ KinectHologram/Assets/Scripts/KinectManager.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+using Microsoft.Kinect.Face;
+using System.Linq;
+
+public class KinectManager : MonoBehaviour
+{
+    public GameObject ErrorMessage;
+
+    private KinectSensor sensor;
+    private BodyFrameReader bodyReader;
+    private FaceFrameReader faceReader;
+    private FaceFrameSource faceSource;
+
+    public Windows.Kinect.Vector4 HeadRotation
+    {
+        get;
+        private set;
+    }
+
+    public CameraSpacePoint HeadPosition
+    {
+        get;
+        private set;
+    }
+
+    public 
+
+    void Start()
+    {
+        this.sensor = KinectSensor.GetDefault();
+        if (this.sensor != null)
+        {
+            this.bodyReader = this.sensor.BodyFrameSource.OpenReader();
+            this.faceSource = FaceFrameSource.Create(this.sensor, 0, FaceFrameFeatures.RotationOrientation);
+            this.faceReader = this.faceSource.OpenReader();           
+
+            if (!sensor.IsOpen)
+            {
+                sensor.Open();
+            }
+        } else
+        {
+            ErrorMessage.SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        if (this.bodyReader != null)
+        {
+            var frame = this.bodyReader.AcquireLatestFrame();
+            if (frame != null)
+            {
+                Body[] bodies = new Body[frame.BodyCount];
+                frame.GetAndRefreshBodyData(bodies);
+                Body body = bodies.Where(b => b.IsTracked).FirstOrDefault();
+
+                if (body != null)
+                {
+                    this.HeadPosition = body.Joints[JointType.Head].Position;
+
+                    if (!this.faceSource.IsTrackingIdValid)
+                    {
+                        this.faceSource.TrackingId = body.TrackingId;
+                    }
+                }
+
+                frame.Dispose();
+                frame = null;
+            }
+        }
+
+        if (this.faceReader != null)
+        {
+            var frame = this.faceReader.AcquireLatestFrame();
+
+            if (frame != null)
+            {
+                FaceFrameResult result = frame.FaceFrameResult;
+                if(result != null)
+                {
+                    this.HeadRotation = result.FaceRotationQuaternion;
+                }
+            }
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        if (this.faceReader != null)
+        {
+            this.faceReader.Dispose();
+            this.faceReader = null;
+        }
+
+        if (this.bodyReader != null)
+        {
+            this.bodyReader.Dispose();
+            this.bodyReader = null;
+        }
+
+        if (this.sensor != null)
+        {
+            if (this.sensor.IsOpen)
+            {
+                this.sensor.Close();
+            }
+
+            this.sensor = null;
+        }
+    }
+}
---
+++ KinectHologram/Assets/Scripts/RotateCharacter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class RotateCharacter : MonoBehaviour
+{
+    public Transform character;
+    public GameObject kinectManagerObject;
+
+    private KinectManager kinectManager;
+    //float degreesPerSecond = 50.0f;
+
+    private void Start()
+    {
+        this.kinectManager = this.kinectManagerObject.GetComponent<KinectManager>();
+    }
+
+
+    void Update()
+    {
+        //Windows.Kinect.Vector4 vector = this.kinectManager.HeadRotation;
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(-90f, 0f, 0f) * new Quaternion(-vector.X, -vector.Z, -vector.Y, vector.W), 0.1f);
+        
+        CameraSpacePoint headPosition = this.kinectManager.HeadPosition;
+        Vector3 headVector = new Vector3(headPosition.X * 100, -headPosition.Y * 100, headPosition.Z * 100);
+
+        Debug.Log(headVector);
+
+        //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(headVector) * Quaternion.Euler(-90f, 0f, 0f), 0.5f);
+        transform.rotation = Quaternion.LookRotation(headVector) * Quaternion.Euler(-90f, 0f, 0f);
+    }
+}
